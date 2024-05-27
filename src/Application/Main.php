59a72f1e94b4c2e50b611185061287bd6a75622c
Jamie Duddleston <guildedthorn@gmail.com>
2024-05-26 22:41:19 -0500

    Screw Jetbrains

--- src/Application/Main.php
+++ src/Application/Main.php
@@ -2,73 +2,35 @@
 
 namespace Application;
 
-use pocketmine\command\{CommandSender, Command};
-use pocketmine\plugin\{PluginBase, Plugin};
+use CortexPE\DiscordWebhookAPI\Webhook;
+use pocketmine\plugin\{PluginBase};
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
-use pocketmine\player\Player;
-use pocketmine\form\FormValidationException;
-use jojoe77777\FormAPI\CustomForm;
 
 class Main extends PluginBase implements Listener{
 
-    private $discordWebhookURL; // Discord webhook URL
+    
+    public static Main $main;
+    public Config $config;
+    public Webhook $webhook; 
 
     public function onEnable(): void {
+        self::$main = $this;
+        if(!is_dir($this->getDataFolder())){
+            @mkdir($this->getDataFolder());
+        }
+        $this->saveDefaultConfig();
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->getLogger()->info("YouTube Application Plugin Activated");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder() . "ytapplication" . DIRECTORY_SEPARATOR);
-
+        
         // Add Discord webhook URL here
-        $this->discordWebhookURL = "https://discord.com/api/webhooks/1228196948872532018/tEyDfeIUI9RHZNIahDc118yEhD6ctWXfUqI1cOJ0qCsiml2ZVEMf2I3Dql4kSME8pPiD";
+        $this->webhook = new Webhook($this->config->get("webhook_url")); 
+        $this->getServer()->getCommandMap()->register("ytapplication", new YTApplicationCommand());
     }
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if($command->getName() == "ytapplication") {
-            if($sender instanceof Player) {
-                $this->applicationForm($sender);
-            } else {
-                $sender->sendMessage("This command can only be used in-game.");
-            }
-        }
-        return true;
-    }
-
-    public function applicationForm(Player $player) {
-        $form = new CustomForm(function (Player $player, ?array $data = null) {
-            if($data === null) return;
-            $config = new Config($this->getDataFolder() . "/ytapplication/" . $player->getName() . ".yml", Config::YAML);
-            $config->set("ytapplication",["Channel: " . $data[0], "Subscribers: " . $data[1], "Views: " . $data[2]]);
-            $config->save();
-            $player->sendMessage("§cYouTube §aApplication Received");
-
-            // Send log to Discord
-            $this->sendDiscordLog($data[0], $data[1], $data[2]);
-        });
-        $form->setTitle("§cYouTube §rApplication");
-        $form->addInput("§8* §cEnter Channel Name Below\n§8* §cThen Select Number of Subscribers\n§8* §cThen Select Number of Views\n\n§cNote: §aIf You Write Offensive Content You Will Be Banned\n\n§cNote: §cIf You Can't Select Your Subscribers or Views, Try to Select the Nearest One", "§7Enter Channel Name Here");
-        $form->addSlider("Subscribers", 1, 1000);
-        $form->addSlider("Views", 1, 1000);
-        $player->sendForm($form);
-    }
-
-    // Function to send log to Discord
-    private function sendDiscordLog(string $channel, int $subscribers, int $views) {
-        $data = [
-            "username" => "YouTube Application Bot", // Sender's name
-            "content" => "Channel: $channel, Subscribers: $subscribers, Views: $views" // Message to be sent
-        ];
-        $options = [
-            "http" => [
-                "header" => "Content-Type: application/json",
-                "method" => "POST",
-                "content" => json_encode($data)
-            ]
-        ];
-        $context = stream_context_create($options);
-        $result = file_get_contents($this->discordWebhookURL, false, $context);
-        if($result === false){
-            $this->getLogger()->error("An error occurred while sending log to Discord.");
-        }
+    
+    public static function getMain() : Main {
+        return self::$main;
     }
 }
--- src/Application/YTApplicationCommand.php
+++ src/Application/YTApplicationCommand.php
@@ -1,1 +1,60 @@
 <?php
+
+namespace Application;
+
+use CortexPE\DiscordWebhookAPI\Embed;
+use CortexPE\DiscordWebhookAPI\Message;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use jojoe77777\FormAPI\CustomForm;
+
+
+class YTApplicationCommand extends Command {
+
+    public function __construct(){
+        parent::__construct("ytapplication", "Register your yt application for partnership", "/ytapplication", ["ytpartner"]);
+        $this->setPermission("pocketmine.group.user");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if($sender instanceof Player) {
+            $this->applicationForm($sender);
+        } else {
+            $sender->sendMessage("This command can only be used in-game.");
+        }
+    }
+
+
+    public function applicationForm(Player $player) {
+        $form = new CustomForm(function (Player $player, ?array $data = null) {
+            if($data === null) return;
+            $config = new Config(Main::getMain()->getDataFolder() . "/ytapplication/" . $player->getName() . ".yml", Config::YAML);
+            $config->set("ytapplication",["Channel: " . $data[0], "Discord: . $data[1]", "Subscribers: " . $data[2], "Views: " . $data[3]]);
+            $config->save();
+            $player->sendMessage("§cYouTube §aApplication Received");
+
+            // Send log to Discord
+            $this->sendDiscordLog($data[0], $data[1], $data[2], $data[3]);
+        });
+        $form->setTitle("§cYouTube §rApplication");
+        $form->addInput("§8* §cEnter Channel Name Below\n§8* §cThen Select Number of Subscribers\n§8* §cThen Select Number of Views\n\n§cNote: §aIf You Write Offensive Content You Will Be Banned\n\n§cNote: §cIf You Can't Select Your Subscribers or Views, Try to Select the Nearest One", "§7Enter Channel Name Here");
+        $form->addInput("§8* §cEnter Discord Username Below");
+        $form->addSlider("Subscribers", 1, 1000);
+        $form->addSlider("Views", 1, 1000);
+        $player->sendForm($form);
+    }
+
+    // Function to send log to Discord
+    private function sendDiscordLog(string $channel, string $discord, int $subscribers, int $views) {
+        $msg = new Message();
+        $msg->setUsername(Main::getMain()->config->get("webhook_name")); // optional
+        $msg->setAvatarURL(Main::getMain()->config->get("webhook_logo")); // optional
+        $embed = new Embed();
+        $embed->setTitle(Main::getMain()->config->get("embed_title"));
+        $embed->setDescription("Channel: $channel, Discord: $discord. Subscribers: $subscribers, Views: $views");
+        $msg->addEmbed($embed);
+        Main::getMain()->webhook->send($msg);
+    }
+}
